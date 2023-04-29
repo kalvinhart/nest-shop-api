c9248f5b24a15a0ce9461d2b8fab75c1946d1d20
Kalvin Hart <kalvinhart@hotmail.co.uk>
2023-04-29 21:27:28 +0100

    fixed missing import

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { Product, ProductSchema } from '../product/schemas/product.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Product.name, schema: ProductSchema },
+    ]),
   ],
   controllers: [UserController],
   providers: [UserService],
